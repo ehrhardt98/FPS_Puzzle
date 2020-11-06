409bd447551b81c32acf697aa3b8ba2ab6fabdd2
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2020-11-06 01:28:16 -0300

    Second iteration of the game

---
+++ SoundManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+
+    public static AudioClip pickup;
+    static AudioSource audioSrc;
+    void Start()
+    {
+        pickup = Resources.Load<AudioClip>("Pickup");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "Pickup":
+                audioSrc.PlayOneShot(pickup);
+                break;
+        }
+    }
+}
